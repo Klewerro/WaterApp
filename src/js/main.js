a3a652e4a1bd7e81d05068edbdac0120a2eed122
PSzymon <pol.szym95@gmail.com>
2019-08-01 22:16:55 +0100

    Saving number of drinked water glasses. Read and write from/to local storage

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 "use strict";
 
+import { isNullOrUndefined } from "util";
+
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
@@ -15,8 +17,48 @@ if ('serviceWorker' in navigator) {
 }
 
 // place your code below
-
-
 console.log(`Hello world!`);
 
+const glassCounter = document.querySelector('.glass__number--js');
+const addGlassButton = document.querySelector('.buttons__add--js');
+const removeGlassButton = document.querySelector('.buttons__remove--js');
+const todayStartDate = new Date().toISOString().slice(0, 10);
+const localStorageInitialValue = localStorage.getItem(todayStartDate);
+
+if (isNullOrUndefined(localStorageInitialValue)) {
+  glassCounter.innerHTML = 0;
+  saveValueToLocalStorage(glassCounter.innerHTML);
+} else {
+  glassCounter.innerHTML = localStorage.getItem(todayStartDate);
+}
+
+
+
+addGlassButton.addEventListener('click', e => addGlass());
+removeGlassButton.addEventListener('click', e => removeGlass());
+
+
+
+
+
+function addGlass() {
+  if (glassCounter.innerHTML < 10) {
+    glassCounter.innerHTML++;
+    saveValueToLocalStorage(glassCounter.innerHTML);
+  }
+    
+}
+
+function removeGlass() {
+  if (glassCounter.innerHTML > 0) {
+    glassCounter.innerHTML--;
+    saveValueToLocalStorage(glassCounter.innerHTML);
+  }
+    
+}
+
+function saveValueToLocalStorage(value) {
+  const dateKey = new Date().toISOString().slice(0, 10);
+  localStorage.setItem(dateKey, value);
+}
 
